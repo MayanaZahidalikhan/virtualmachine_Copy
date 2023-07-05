608be7232a7042850e5d31b5e93c7ecfd1683360
MayanaZahidalikhan <khanshanu457@gmail.com>
2023-07-05 10:43:08 -0400

    push the code

---
+++ virtualmachinecreation/src/main/java/com/azure/virtualmachinecreation/VirtualmachinecreationApplication.java
@@ -0,0 +1,13 @@
+package com.azure.virtualmachinecreation;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VirtualmachinecreationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VirtualmachinecreationApplication.class, args);
+	}
+
+}
---
+++ virtualmachinecreation/src/test/java/com/azure/virtualmachinecreation/VirtualmachinecreationApplicationTests.java
@@ -0,0 +1,13 @@
+package com.azure.virtualmachinecreation;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VirtualmachinecreationApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
